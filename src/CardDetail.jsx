a6f3a14cb30c9b8ad92b81c2f5a4e775d211cac8
Meghla15 <meherunnesa1505@gmail.com>
2024-03-26 05:08:09 +0600

    listed book added

--- src/CardDetail.jsx
+++ src/CardDetail.jsx
@@ -3,10 +3,15 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useBooksCartData from "./Hooks/useBooksCartData";
 
+
 const CardDetail = () => {
    const [singleData, setSingleData] =useState({});
    const {id} = useParams();
-   const {cardData} =useBooksCartData();
+   const {cardData,loading} =useBooksCartData();
+
+   const handleClick =()=>{
+    
+   }
    
    useEffect(() =>{
     if (cardData) {
@@ -20,7 +25,7 @@ const CardDetail = () => {
     return (
     <section className="dark:bg-gray-100 dark:text-gray-800">
       <div className="container max-w-6xl p-6 mx-auto  sm:space-y-12">
-        <a
+      <a
           rel="noopener noreferrer"
           href="#"
           className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50"
@@ -28,7 +33,7 @@ const CardDetail = () => {
           <img
             src={image}
             alt=""
-            className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500"
+            className="w-full h-full rounded sm:h-96 lg:col-span-6 dark:bg-gray-500"
           />
           <div className="p-6 space-y-2 lg:col-span-5">
             <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
@@ -49,8 +54,8 @@ const CardDetail = () => {
             <p>Publisher : <span className="font-bold">{publisher}</span></p>
             <p>Year of Publishing : <span className="font-bold">{publication_year}</span></p>
             <p>Rating : <span className="font-bold">{rating}</span></p>
-            <button>Read</button>
-            <button>WishList</button>
+            <button onClick={handleClick()} className="btn btn-ghost">Read</button>
+            <button  className="btn btn-ghost">WishList</button>
           </div>
         </a>
         
---
+++ src/Utiles/localStorage.js
@@ -0,0 +1,15 @@
+// export const saveToLocalStorage = (cardData) => {
+   
+       
+//         const savedData = JSON.parse(localStorage.getItem("read") || "[]");
+//         const existedData = savedData.find((item) => item.id === cardData.id);
+
+//         if (!existedData) {
+//             savedData.push(cardData);
+//             localStorage.setItem("read", JSON.stringify(savedData));
+//             alert("Added successfully!");
+//         } else {
+//             alert("Already exists!");
+//         }
+   
+// };
